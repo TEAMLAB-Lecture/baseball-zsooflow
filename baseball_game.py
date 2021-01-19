344776a70f3a924071251ae870b68d0aa8c3e8d8
jisukim <jisu2052@naver.com>
2021-01-19 18:03:46 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -102,7 +102,7 @@ def is_duplicated_number(three_digit):
         if num not in new_list:
             new_list.append(num)
     
-    result = three_digit == new_list
+    result = not (three_digit == new_list)
 
     # ==================================
     return result
@@ -130,7 +130,7 @@ def is_validated_number(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    result = is_digit(user_input_number) and is_between_100_and_999(user_input_number) and is_duplicated_number(user_input_number)
+    result = is_digit(user_input_number) and is_between_100_and_999(user_input_number) and not is_duplicated_number(user_input_number)
     # ==================================
     return result
 
